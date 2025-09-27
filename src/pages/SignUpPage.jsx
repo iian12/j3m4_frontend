4c13128519f1f7cd5c128fbbac12c9530acd146a
jsj03 <jsj03529@gmail.com>
2025-09-27 14:40:16 +0900

    전화번호 필드 추가

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -10,6 +10,7 @@ const SignUpPage = () => {
         password: '',
         name: '',
         email: '',
+        phonenumber: ''
     });
     const navigate = useNavigate();
 
@@ -76,6 +77,17 @@ const SignUpPage = () => {
                         id="email"
                         name="email"
                         value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="phonenumber">전화번호</label>
+                    <input
+                        type="phonenumber"
+                        id="phonenumber"
+                        name="phonenumber"
+                        value={formData.phonenumber}
                         onChange={handleChange}
                         required
                     />
