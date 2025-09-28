2202727ac274ca306af739d9bf9eee05be5c610c
minchan <minchan3192@gmail.com>
2025-09-28 23:54:42 +0900

    fix CORS problem

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -2,4 +2,4 @@ import api from "./axios";
 
 
 export const login = (data) => api.post("/auth/login", data);
-export const signup = (data) => api.post("/auth/signup", data);
+export const signup = (data) => api.post("/users/join", data);
